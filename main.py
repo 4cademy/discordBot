b0d3639a146fe4223a27d48318d4c783bbccf559
mabe447c <marcel.beyer2@mailbox.tu-dresden.de>
2023-11-06 15:16:13 +0100

    Added emoji to message

--- main.py
+++ main.py
@@ -9,10 +9,17 @@ intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
+emoji = '<:bot_box:1171090169257017344>'
+
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+
+
+@bot.command(name='hello')
+async def ping(ctx):
+    await ctx.channel.send(f'Hello {emoji}')
 
 
 @bot.command(name='ping', aliases=['PING'])
@@ -29,7 +36,7 @@ async def add(ctx, a, b):
 @add.error
 async def add_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('Fehler: Es fehlen erforderliche Argumente. $add <Zahl1> <Zahl2>')
+        await ctx.send('Fehler: Es fehlen erforderliche Argumente. Format: $add <Zahl1> <Zahl2>')
 
 
 TOKEN = os.getenv('TOKEN')
