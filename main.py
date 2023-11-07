217b98ea58c5a9973bd3fcdc22c9a5a595ec4311
mabe447c <marcel.beyer2@mailbox.tu-dresden.de>
2023-11-07 16:33:30 +0100

    Finished tasks of session 3

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
+
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
@@ -34,11 +35,14 @@ async def add(ctx, a, b):
 @add.error
 async def add_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('Fehler: Es fehlen erforderliche Argumente. Format: **$add <Zahl1> <Zahl2>**')
+        await ctx.send('Fehler: Fehlendes erforderliches Argumente. Format: **$add <Zahl1> <Zahl2>**')
 
 
 @bot.command(name='münzwurf', aliases=['Münzwurf', 'MÜNZWURF'])
 async def muenzwurf(ctx, wahl):
+    if wahl != 'Kopf' and wahl != 'Zahl':
+        raise commands.BadArgument
+
     await ctx.channel.send(f'Münze wird geworfen...')
     zahl = random.randint(0, 1)
     if zahl == 0:
@@ -57,7 +61,42 @@ async def muenzwurf(ctx, wahl):
 @muenzwurf.error
 async def muenzwurf_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('Fehler: Es fehlen erforderliche Argumente. Format: **$münzwurf <Kopf/Zahl>**')
+        await ctx.send('Fehler: Fehlendes erforderliches Argumente. Format: **$münzwurf <Kopf/Zahl>**')
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send('Fehler: Ungültiges Argument. Format: **$münzwurf <Kopf/Zahl>**')
+
+
+@bot.command(name='ssp')
+async def ssp(ctx, wahl):
+    valid_args = ['Schere', 'Stein', 'Papier']
+    emojis = [':scissors:', ':rock:', ':newspaper:']
+    if wahl not in valid_args:
+        raise commands.BadArgument
+
+    user_no = valid_args.index(wahl)
+
+    com_no = random.randint(0, 2)
+
+    await ctx.channel.send(f'{emojis[user_no]} vs {emojis[com_no]}')
+
+    if user_no == 0 and com_no == 2:
+        await ctx.channel.send(f'Du hast gewonnen!')
+    elif user_no == 2 and com_no == 0:
+        await ctx.channel.send(f'Du hast verloren!')
+    elif user_no > com_no:
+        await ctx.channel.send(f'Du hast gewonnen!')
+    elif user_no < com_no:
+        await ctx.channel.send(f'Du hast verloren!')
+    else:
+        await ctx.channel.send(f'Unentschieden!')
+
+
+@ssp.error
+async def ssp_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Fehler: Fehlendes erforderliches Argumente. Format: **$ssp <Schere/Stein/Papier>**')
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send('Fehler: Ungültiges Argument. Format: **$ssp <Schere/Stein/Papier>**')
 
 
 TOKEN = os.getenv('TOKEN')
